2039abe60003d80f2e6e01ba285cab5d594e9bbd
Kleber Molica <kleber.molica@gmail.com>
2013-07-10 22:58:21 +0100

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
+var fs = require('fs');
+
+fs.readFileSync('index.html', function(err, data) {
+  if (err) throw err;
+  buf = new Buffer(data.length);
+  len = buf.write(data, 0, 'utf-8');
+  console.log(data);
+}
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString('utf8', 0, len));
 });
 
 var port = process.env.PORT || 5000;
